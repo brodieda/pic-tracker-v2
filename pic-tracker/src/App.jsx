6851a17a922160d88414f37e55c9e2deddbf1a7c
brodieda <brodied@nuaa.org.au>
2026-07-23 20:28:45 +1000

    Add files via upload

--- pic-tracker/src/App.jsx
+++ pic-tracker/src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import EventSettings from './components/EventSettings'
 import CareBoard from './components/CareBoard'
 import FloorCheck from './components/FloorCheck'
