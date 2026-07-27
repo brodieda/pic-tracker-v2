1ddf854b2143d4cce8fd4794372397c28da97e72
brodieda <brodied@nuaa.org.au>
2026-07-27 10:41:22 +1000

    Add files via upload

--- pic-tracker/src/main.jsx
+++ pic-tracker/src/main.jsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { initTheme } from './lib/theme'
+import { applyTeamColors } from './lib/teamColors'
+import { getEvent } from './lib/store'
 
-// Apply stored theme + size before first paint to avoid flash of wrong theme
+// Apply stored theme + size + team colours before first paint to avoid a flash
 initTheme()
+applyTeamColors(getEvent())
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
